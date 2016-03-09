e346a591ee41af354aa6402a0e1c9be1e4269963
jciarallij <jciarallij@gmail.com>
2016-03-09 17:59:27 -0500

    init commit
    a lot more progress! My tic tac toe works!

--- js/scripts.js
+++ js/scripts.js
@@ -1,109 +1,133 @@
 
 var winners =[
-// Horz Answers
+// horizontal Answers
 	['a1', 'a2', 'a3'],
 	['b1', 'b2', 'b3'],
 	['c1', 'c2', 'c3'],
-// Vert Answers
+// vertical Answers
 	['a1', 'b1', 'c1'],
 	['a2', 'b2', 'c2'],
 	['a3', 'b3', 'c3'],
-// Diag Answers
+// diagnol Answers
 	['a1', 'b2', 'c3'],
 	['c1', 'b2', 'a3'],
 ];
+
 var whosTurn = 1;
 var playerOneMarkings = [];
 var playerTwoMarkings = [];
-var buttonHeader = document.getElementById('message');
+var gameHeader = document.getElementById('message');
+var playerMode;
+var computer;
 
-function addSymbol(element) {  /////  ***** This ADD'S SYMBOLS ****** //////
+function onePlayer(){
+	computer = true;
+	document.getElementById
+	var playerMode = 1;
+
+	var box = document.getElementsByClassName('box');
+	for(i=0; i<box.length; i++) {
+		box[i].style.pointerEvents = 'auto';
+	}
+
+}
+
+function twoPlayer(){
+	computer = false;
+	playerMode = 2;
+	document.getElementById('message').innerHTML = 'Player 1\'s turn!';
+	var box = document.getElementsByClassName('box');
+	for(i=0; i<box.length; i++) {
+		box[i].style.pointerEvents = 'auto';
+	}
+}
+function addSymbol(element) {  
 	
-	if(element.innerHTML == '') { //This square is empty
+	if(element.innerHTML == '') { 
 		if(whosTurn == 1) {
 			element.innerHTML = 'X';		
 			whosTurn = 2;
-			buttonHeader.innerHTML = "Player 2's Turn";
-			buttonHeader.className = 'player-2';
-			element.classList.remove('empty');  //Get rid of class 'empty', and add who took the square.
-			element.classList.add('p1');  // adds an element to the array
+			gameHeader.innerHTML = "Player 2's Turn";
+			gameHeader.className = 'player-2';
+			element.classList.remove('empty');  
+			element.classList.add('p1'); 
 			playerOneMarkings.push(element.id);
-			// checkWin();
-
-			// If player chooses '1 Player'
-			// computersTurn();
+			checkWin();
+			if(computer == true){
+				computersTurn();
+			}
+		
 		}
 		else {
-			element.innerHTML = 'O';		// It has to be O's turn. Put an O in,
+			element.innerHTML = 'O';		
 			whosTurn = 1;
-			buttonHeader.innerHTML = "Player 1's Turn";
-			buttonHeader.className = 'player-1';
+			gameHeader.innerHTML = "Player 1's Turn";
+			gameHeader.className = 'player-1';
 			element.classList.remove('empty');
 			element.className += ' p2';         
 			playerTwoMarkings.push(element.id);	
 		}
 	}
 	else {
-		buttonHeader.innerHTML = "Box is taken";
-		buttonHeader.className = 'red';
+		gameHeader.innerHTML = "Box is taken";
+		gameHeader.className = 'red';
 	}
 
 	checkWin();
 }
 
 
-// function computersTurn(){
-// 	//It has to be O's turn. Put an O in.
-// 	// Get a random, empty square.
-// 	var arrayOfEmptySquares = document.getElementsByClassName('empty');
-// 	var randomEmptySquareIndex = Math.floor(Math.random() * arrayOfEmptySquares.length);
-// 	var element = arrayOfEmptySquares[randomEmptySquareIndex];
-// 	element.innerHTML = 'O';
-// 	whosTurn = 1;
-// 	gameHeader.innerHTML = "It is Player 1's turn";
-// 	gameHeader.className = 'player-one';
-// 	element.classList.remove('empty');
-// 	element.classList.add('p2');
-// 	playerTwoMarkings.push(element.id);	
-// 	checkWin();
-// }
+function computersTurn(){
+	//It has to be O's turn. Put an O in.
+	// Get a random, empty square.
+	var arrayOfEmptySquares = document.getElementsByClassName('empty');
+	var randomEmptySquareIndex = Math.floor(Math.random() * arrayOfEmptySquares.length);
+	var element = arrayOfEmptySquares[randomEmptySquareIndex];
+	element.innerHTML = 'O';
+	whosTurn = 1;
+	gameHeader.innerHTML = "It is Player 1's turn";
+	gameHeader.className = 'player-one';
+	element.classList.remove('empty');
+	element.classList.add('p2');
+	playerTwoMarkings.push(element.id);	
+	checkWin();
+}
 
 
 
 
 
-function checkWin() {       /////  ***** This CHECKS this win ****** //////
+function checkWin() {     
 
 
+console.log(playerOneMarkings);
+console.log(playerTwoMarkings);
 	
-	var rowCount = 0;  // Loop through all winning possilities. RowCount nees to restart each time.
+	var rowCount = 0;  
 	var rowCountTwo = 0;
 	var thisWinCombo;
 	
-	for (i=0; i<winners.length; i++){
+	for (var i=0; i<winners.length; i++){
 		rowCount = 0;
 		rowCountTwo = 0;
 		thisWinCombo = winners[i];
-		
-		for (j = 0; j < thisWinCombo.length; j++) { // Now, let's check if all the elements in the winners array, exist in the current player array.
-			if (playerOneMarkings.indexOf(thisWinCombo[j]) > -1) { // Check if this box of the win combo we are on, is in the player's marking
-				rowCount++;
 
+		for (j = 0; j < thisWinCombo.length; j++) {
+			if (playerOneMarkings.indexOf(thisWinCombo[j]) > -1) { 
+				rowCount++;
 			}
-			if (rowCount === 3){
-				gameOver(thisWinCombo, 1);
-			}
-		}
 			if (playerTwoMarkings.indexOf(thisWinCombo[j]) > -1) {
 				rowCountTwo++;
-
-			
-
 			}
-			 if(rowCountTwo === 3){
-				gameOver(thisWinCombo, 2);
-			}	break;
-	
+		}
+		if (rowCount === 3){
+			gameOver(thisWinCombo, 1);
+			break;
+		}
+		 if(rowCountTwo === 3){
+			gameOver(thisWinCombo, 2);
+			break;
+		}	
 	}
 
 }
@@ -113,19 +137,12 @@ function gameOver(combo,playerWinner) {
 	for(i=0; i<combo.length; i++){
 		document.getElementById(combo[i]).classList.add('winner');
 	}
-	buttonHeader.innerHTML = 'Player ' + playerWinner + ' , won the game!';
+	if(playerWinner == 2){
+		gameHeader.className = 'player-2';
+		document.getElementById('replay').style.display = 'block';		
+	}
+	gameHeader.innerHTML = 'Player ' + playerWinner + ' , won the game!';
+	document.getElementById('replay').style.display = 'block';	
 }
-// 	var replay = document.getElementsByTagName("button");
-// 	for(i=0; i<buttons.length; i++){
-// 		buttons[i].disabled = true;
-
-// 		buttons[i].style.pointerEvents = 'none';
-// 	}
 
 
-// function resetGame() {
-// 	Clear Player Arrays
-// 	Clear innerHTML of emptySquares
-// 	Updates wins to counter for the winning playerOneMarkings
-// 	Undisable the boxes
-// }
