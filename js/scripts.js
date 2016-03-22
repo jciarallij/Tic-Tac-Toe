ab061eee416ccd9be3c6dff1063c0260701a76a6
jciarallij <jciarallij@gmail.com>
2016-03-22 07:01:52 -0400

    init commit
    doing coding golf and compressing my code and using jQuery lib.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// ----------------------------------------- Declaring Global Variables --------------------------------
 
 var winners =[
 // horizontal Answers
@@ -8,49 +9,81 @@ var winners =[
 	['a1', 'b1', 'c1'],
 	['a2', 'b2', 'c2'],
 	['a3', 'b3', 'c3'],
-// diagnol Answers
+// diagonal Answers
 	['a1', 'b2', 'c3'],
 	['c1', 'b2', 'a3'],
 ];
-
 var whosTurn = 1;
 var playerOneMarkings = [];
 var playerTwoMarkings = [];
-var gameHeader = document.getElementById('message');
+var gameHeader = $('#message');
 var playerMode;
 var computer;
 
+// ----------------------------------------- Click Function ---------------------------------------------
+
+$(document).ready(function(){
+	$('button').click(function(){
+		var clickedButton = ($(this).attr('id'));
+		if(clickedButton == 'player-one'){
+			// console.log('#player-one');
+			onePlayer();
+		}else if(clickedButton == 'player-two'){
+			// console.log('#player-two');
+			twoPlayer();
+		}else if(clickedButton == 'replay'){
+			replay();
+		
+		};
+	});
+
+	$('.box').click(function(){
+			// console.log(this);
+			addSymbol(this);
+	})
+
+
+});
+
+// ----------------------------------------- One Players ---------------------------------------------
+
 function onePlayer(){
 	computer = true;
-	document.getElementById
 	var playerMode = 1;
-
-	var box = document.getElementsByClassName('box');
+	var box = $('.box');
 	for(i=0; i<box.length; i++) {
 		box[i].style.pointerEvents = 'auto';
 	}
-
+	// $('player-one').prop('disabled',true);
+	// $('player-two').prop('disabled',true);
 }
+
+// ----------------------------------------- Two Players ---------------------------------------------
 
 function twoPlayer(){
 	computer = false;
 	playerMode = 2;
-	document.getElementById('message').innerHTML = 'Player 1\'s turn!';
-	var box = document.getElementsByClassName('box');
+	$('#message').innerHTML = 'Player 1\'s turn!';
+	var box = $('.box');
 	for(i=0; i<box.length; i++) {
 		box[i].style.pointerEvents = 'auto';
 	}
+	// $('player-one').prop('disabled',true);
+	// $('player-two').prop('disabled',true);
 }
-function addSymbol(element) {  
-	
+
+// ----------------------------------------- Add Symbols to board ------------------------------------
+
+function addSymbol(element){  
+	console.log(element)
 	if(element.innerHTML == '') { 
 		if(whosTurn == 1) {
-			element.innerHTML = 'X';		
+			element.innerHTML ='X';		
 			whosTurn = 2;
-			gameHeader.innerHTML = "Player 2's Turn";
-			gameHeader.className = 'player-2';
-			element.classList.remove('empty');  
-			element.classList.add('p1'); 
+			gameHeader.html("Player 2's Turn");
+			gameHeader.addClass('player-2');
+			$(element).removeClass('.empty');  
+			$(element).addClass('.p1'); 
 			playerOneMarkings.push(element.id);
 			checkWin();
 			if(computer == true){
@@ -59,58 +92,52 @@ function addSymbol(element) {
 		
 		}
 		else {
-			element.innerHTML = 'O';		
+			element.innerHTML ='O';		
 			whosTurn = 1;
-			gameHeader.innerHTML = "Player 1's Turn";
-			gameHeader.className = 'player-1';
-			element.classList.remove('empty');
-			element.className += ' p2';         
+			gameHeader.html("Player 1's Turn");
+			gameHeader.addClass('player-1');
+			$(element).removeClass('empty');
+			$(element).addClass(' p2');         
 			playerTwoMarkings.push(element.id);	
 		}
 	}
 	else {
-		gameHeader.innerHTML = "Box is taken";
-		gameHeader.className = 'red';
+		gameHeader.html("Box is taken");
+		gameHeader.addClass('red');
 	}
 
 	checkWin();
 }
 
+// ----------------------------------------- Computers Turn ----------------------------------------
 
 function computersTurn(){
 	//It has to be O's turn. Put an O in.
 	// Get a random, empty square.
-	var arrayOfEmptySquares = document.getElementsByClassName('empty');
+	var arrayOfEmptySquares = $('.empty');
 	var randomEmptySquareIndex = Math.floor(Math.random() * arrayOfEmptySquares.length);
 	var element = arrayOfEmptySquares[randomEmptySquareIndex];
 	element.innerHTML = 'O';
 	whosTurn = 1;
 	gameHeader.innerHTML = "It is Player 1's turn";
-	gameHeader.className = 'player-one';
-	element.classList.remove('empty');
-	element.classList.add('p2');
+	gameHeader.addClass('player-one');
+	$(element).removeClass('empty');
+	$(element).addClass('p2');
 	playerTwoMarkings.push(element.id);	
 	checkWin();
 }
 
-
-
-
+// ----------------------------------------- Check Win ---------------------------------------------
 
 function checkWin() {     
-
-
-console.log(playerOneMarkings);
-console.log(playerTwoMarkings);
-	
 	var rowCount = 0;  
 	var rowCountTwo = 0;
 	var thisWinCombo;
-	
 	for (var i=0; i<winners.length; i++){
 		rowCount = 0;
 		rowCountTwo = 0;
 		thisWinCombo = winners[i];
+
 
 		for (j = 0; j < thisWinCombo.length; j++) {
 			if (playerOneMarkings.indexOf(thisWinCombo[j]) > -1) { 
@@ -127,22 +154,26 @@ console.log(playerTwoMarkings);
 		 if(rowCountTwo === 3){
 			gameOver(thisWinCombo, 2);
 			break;
-		}	
+		}
+
 	}
 
 }
 
+// ----------------------------------------- Game Over ---------------------------------------------
 
 function gameOver(combo,playerWinner) {
-	for(i=0; i<combo.length; i++){
-		document.getElementById(combo[i]).classList.add('winner');
+	for(i=0; i<$(combo).length; i++){
+		$(combo[i]).addClass('winner');
 	}
 	if(playerWinner == 2){
-		gameHeader.className = 'player-2';
-		document.getElementById('replay').style.display = 'block';		
+		gameHeader.addClass('player-2');
+		// $('#replay').show();		
 	}
 	gameHeader.innerHTML = 'Player ' + playerWinner + ' , won the game!';
-	document.getElementById('replay').style.display = 'block';	
+	// $('#replay').show();	
 }
 
+// ----------------------------------------- Score Function ----------------------------------------
 
+// ----------------------------------------- Tie Function ------------------------------------------
